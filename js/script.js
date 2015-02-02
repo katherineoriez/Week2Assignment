d58b342d29488059444c10818d3e7c3828701b4a
The Most Awesome of Awesome <KatherineOriez@mwc-146126.dhcp.missouri.edu>
2015-02-02 14:40:12 -0600

    Week2Assignment test

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
 });
 
 
+
+function getData () {
+	$.getJSON("js/JayBruceStats.json", function (data, error) {
+		playerData = data;
+		drawStuff();	  
+	});
+	
+  }
+
+function drawStuff() {
+	var lastSeasonStats = playerData.stats[4];
+	var lastSeasonRBI = lastSeasonStats.RBI;
+	var lastSeasonYear = lastSeasonStats.year;
+	
+	$(".chart").html("<p>"+playerData.name+"</p>");
+	$(".chart").append("<p>"+playerData.club+"</p>");
+	$(".chart").append("<p>In "+lastSeasonYear+" he hit "+lastSeasonRBI+" RBIs</p>");
+	
+	};
 
 
 
